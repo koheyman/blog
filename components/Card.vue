1a90355bf9507ad516cee90016414c14e88386eb
koheyman <koheyman@gmail.com>
2020-10-10 11:16:41 +0900

    fix:一覧カードにカテゴリ表記追加

--- components/Card.vue
+++ components/Card.vue
@@ -7,6 +7,7 @@
         </div>
         <div class="c-article-card__overview">
             <div class="c-article-card__head">
+              <span class="c-article-card__category">{{ Content.category && Content.category.name }}</span>
               <span class="c-article-card__date" v-text="$dayjs(`${Content.publishedAt}`).format('YYYY/MM/DD')"></span>
             </div>
             <div class="c-article-card__title"><h2>{{ Content.title }}</h2></div>
--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@ export default {
         headers: { 'X-API-KEY': 'e885d50d-8291-48d1-9664-d5cbbc4c3982' }
       }
     )
-    // console.log(data)
+    console.log(data)
     return data
   }
 
