dc6284fe96f946b4d5a154f1feb7e94b4bb2c3dc
koheyman <koheyman@gmail.com>
2020-10-29 06:26:16 +0900

    fix:ogp.pngの参照パス変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 const title = 'WEB FRONTEND BLOG'
 const description = 'フロントエンドでなんとかがんばっていくブログ'
 const url = 'https://oi-tech.blog/'
-const ogImage = `${url}static/ogp.png`
+const ogImage = `${url}ogp.png`
 
 import axios from 'axios'
 require('dotenv').config();
