d8ef5a5b514a56b12b1d23326ea4bc07030dd0f8
koheyman <koheyman@gmail.com>
2020-11-07 13:29:35 +0900

    fix:更新日比較判定修正

--- components/Card.vue
+++ components/Card.vue
@@ -12,7 +12,7 @@
                 <CalendarIcon />
                 <span class="c-article-card__date" v-text="$dayjs(`${Content.publishedAt}`).format('YYYY/MM/DD')"></span>
               </div>
-              <div class="updatedAt" v-if="Content.publishedAt != Content.updatedAt">
+              <div class="updatedAt" v-if="$dayjs(`${Content.publishedAt}`).format('YYYY/MM/DD') != $dayjs(`${Content.updatedAt}`).format('YYYY/MM/DD')">
                 <UpdateIcon />
                 <span class="c-article-card__date" v-text="$dayjs(`${Content.updatedAt}`).format('YYYY/MM/DD')"></span>
               </div>
